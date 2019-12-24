640f062bbbe19261888f6c1a999d84c3c09c83ba
KelvinMitaki <mitakikelvin1@gmail.com>
2019-12-24 13:42:46 +0300

    New Commits added

--- client/src/components/view-profile/ViewProfile.js
+++ client/src/components/view-profile/ViewProfile.js
@@ -15,12 +15,38 @@ export class ViewProfile extends Component {
     }
   }
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            experience={profile.experience}
+            education={profile.education}
+          />
+          <ProfileGithub />
+        </div>
+      );
+    }
     return (
-      <div>
-        <ProfileHeader />
-        <ProfileGithub />
-        <ProfileAbout />
-        <ProfileCreds />
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
