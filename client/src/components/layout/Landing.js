20e236e031fddfc28d70f63cf37ce375339dc231
KelvinMitaki <mitakikelvin1@gmail.com>
2019-12-27 14:15:32 +0300

    new changes added

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -18,7 +18,7 @@ export class Landing extends Component {
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Geek Hub</h1>
                 <p className="lead"> Create a developer profile/portfolio</p>
-                <p>Share posts and connect with other developers</p>
+                <p>Share posts and get help from other developers</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
