cf83e7b45d6f5db64d6bbfb3d7a52037d30ef74e
KelvinMitaki <mitakikelvin1@gmail.com>
2019-12-27 17:52:35 +0300

    new

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -57,7 +57,7 @@ export class Navbar extends Component {
     return (
       <nav className="navbar sticky-top navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="#/">
             GeekHub
           </Link>
           <button
